a5c0a997a9b8b684754ac5eed16a38bd141cd4dc
杨桃 <58856809+yot777@users.noreply.github.com>
2020-02-03 21:31:56 +0800

    Add files via upload

---
+++ Chapter5/5.1Test1.java
@@ -0,0 +1,10 @@
+public class Test1 {
+public static void main(String[] args) {
+        int a=5;
+        char b='A';
+        String c="Test";
+        System.out.println(a);
+        System.out.println(b);   
+        System.out.println(c);
+        }
+}  
